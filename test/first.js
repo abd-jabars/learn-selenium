9fca19eb6b456cacc729f43597143d758a155fba
Syed Abdul Jabar <7abdjabar@gmail.com>
2022-04-24 17:03:58 +0700

    using mocha framework

--- test/first.js
+++ test/first.js
@@ -3,38 +3,39 @@ const {
   By,
   Key
 } = require("selenium-webdriver");
+
+// web-driver
 const chrome = require('selenium-webdriver/chrome');
 const chromedriver = require('chromedriver');
-const assert = require("assert"); // built in node js assertion library
+const firefox = require('selenium-webdriver/firefox');
+const firefoxdriver = require('geckodriver');
+
+// assertion library
 const chai = require("chai");
 const should = chai.should();
 
-async function myFirstTest() {
-  // launch the browser
-  // const driver = await new Builder().forBrowser("firefox").build();
-  const driver = await new Builder().forBrowser("chrome").build();
+describe("My First Test", () => {
+  it("successfully add new list", async () => {
+    // launch the browser
+    // const driver = await new Builder().forBrowser("firefox").build();
+    const driver = await new Builder().forBrowser("chrome").build();
 
-  // navigate to apps (url)
-  await driver.get("https://lambdatest.github.io/sample-todo-app/");
+    // navigate to apps (url)
+    await driver.get("https://lambdatest.github.io/sample-todo-app/");
 
-  // todo
-  // find element by id to locate the text box, then input "Learn Selenium" and hit Enter
-  await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
+    // todo
+    // find element by id to locate the text box, then input "Learn Selenium" and hit Enter
+    await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
 
-  // assertion
-  let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(value => {
-    return value;
+    // assertion
+    let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(value => {
+      return value;
+    });
+
+    // assert using chai assertion
+    todoText.should.equal("Learn Selenium");
+
+    // close the browser
+    await driver.quit();
   });
-
-  // assert using node js assertion
-  assert.strictEqual(todoText, "Learn Selenium");
-
-  // assert using chai assertion
-  todoText.should.equal("Learn Selenium");
-
-  // close the browser
-  await driver.quit();
-
-}
-
-myFirstTest();
+});
